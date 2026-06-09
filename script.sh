f0bda5a2235288ba0adfe00524f3a7750be25397
João Russo <40304274+JoaoRuss0@users.noreply.github.com>
2026-06-09 01:56:02 +0100

    feat: add terraform

--- script.sh
+++ script.sh
@@ -2,13 +2,14 @@
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo >> ~/.zprofile
+echo >> $HOME/.zprofile
 echo 'eval "$(/opt/homebrew/bin/brew shellenv zsh)"' >> /Users/$USER/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv zsh)"
 
 # Install Software
+brew tap hashicorp/tap
 brew install --cask ghostty discord firefox jetbrains-toolbox docker-desktop
-brew install maven docker-compose awscli
+brew install maven docker-compose awscli hashicorp/tap/terraform
 
 # .config/ghostty/config.ghostty
 mkdir -p .config/ghostty/
@@ -16,7 +17,7 @@ echo "shell-integration-features = sudo
 theme = Apple System Colors Light" > .config/ghostty/config.ghostty
 
 # .ssh/config
-mkdir .ssh
+mkdir $HOME/.ssh/
 echo "Host *
   UseKeychain yes
   SetEnv TERM=xterm-256color" > .ssh/config
@@ -28,11 +29,16 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # Install SDKMAN
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
-sed -i '' 's/^#*sdkman_auto_env=.*/sdkman_auto_env=true/' ~/.sdkman/etc/config
+sed -i '' 's/^#*sdkman_auto_env=.*/sdkman_auto_env=true/' $HOME/.sdkman/etc/config
 
 # Install cargo (rust)
 curl https://sh.rustup.rs -sSf | sh
 source .zshenv
+
+# Terraform
+mkdir -p $HOME/.terraform.d/plugin-cache/
+echo "plugin_cache_dir   = "$HOME/.terraform.d/plugin-cache"
+disable_checkpoint = true" > $HOME/.terraformrc
 
 git config --global user.name "João Russo"
 git config --global user.email "joaorusso.net@gmail.com"
@@ -48,4 +54,4 @@ git config --global user.email "joaorusso.net@gmail.com"
     # Settings -> Sound -> Disable: Play sound on startup
     # Finder -> Settings -> Advanced -> Search the Current Folder
 
-mkdir ~/Developer
+mkdir $HOME/Developer
